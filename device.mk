e31f50ce0967ac5e1604b58ef99a308e50884ee4
ManshuTyagi <himanshut567@gmail.com>
2026-04-03 11:47:59 +0200

    op6893: Build vendor version of android.hardware.gnss-V1-ndk from source
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.gnss-service.mediatek": library "android.hardware.gnss-V1-ndk_platform.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -211,6 +211,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor:64 \
     android.hardware.gnss@2.0.vendor:64 \
     android.hardware.gnss@2.1.vendor:64 \
+    android.hardware.gnss-V1-ndk.vendor \
     libcurl.vendor
 
 # HIDL
