f09d926b61f67d122845e964f68ca8e85b387c63
Harshit Jain <dev-harsh1998@hotmail.com>
2026-04-06 11:19:02 +0200

    op6893: Build sensors.oplus and add overlay for screenoff udfps

--- device.mk
+++ device.mk
@@ -451,7 +451,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge \
-    libpower.vendor
+    libpower.vendor \
+    sensors.oplus
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
